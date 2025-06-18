9cc8d4c8edfec3b46a02ea7d21c47edf4c62272a
Phước Thành <thanhgithub123@gmail.com>
2025-06-18 22:50:28 +0700

    Chỉnh sửa

--- router/cart.router.js
+++ router/cart.router.js
@@ -8,6 +8,6 @@ router.get("/", authenticateToken, getCart);
 router.post("/add", authenticateToken, addToCart);
 router.put("/update", authenticateToken, updateCart);
 router.delete("/delete", authenticateToken, deleteCart);
-router.delete(":id", authenticateToken, deleteCartById);
+router.delete("/:id", authenticateToken, deleteCartById);
 
 module.exports = router;
